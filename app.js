dc9ba43730069a3ee2e799ab4dcb15c95b580c99
Robyn Alex <robyn.alexander1111@gmail.com>
2022-04-08 01:57:25 -0500

    git comm1t

--- app.js
+++ app.js
@@ -2,3 +2,48 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+/*
+function watchTurorialCallback(callback, errorCallback) {
+    let userLeft = false;
+  
+    if (userLeft) {
+      errorCallback("User left.");
+    } else {
+      callback("Thumbs up and Subscribe!");
+    }
+  }
+  
+  watchTurorialCallback(
+    (message) => {
+      console.log(message);
+    },
+    (error) => {
+      console.log(error.name + " " + error.message);
+    }
+  );
+  */
+
+async function watchTurorialCallback(callback, errorCallback) {
+    let userLeft = Math.round(Math.random());
+
+    if (userLeft) {
+        throw "User left.";
+    } else {
+        return "Thumbs up and subscribe.";
+    }
+}
+
+
+async function giveStatusMessage() {
+    try {
+    let message = await watchTurorialCallback();
+    console.log(message);
+} catch (error) {
+    console.log(error);
+}
+}
+
+
+
+giveStatusMessage();
